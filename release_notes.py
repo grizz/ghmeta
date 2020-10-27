3a6c2ac73f7e9f71f069bfbb747aa0cc86ce314c
Elliot Frank <elliot.g.frank@gmail.com>
2020-10-27 14:41:22 -0500

    Add docstrings and improve error handling

--- release_notes.py
+++ release_notes.py
@@ -3,6 +3,8 @@ import argparse
 import os
 import re
 from datetime import datetime
+import logging
+
 
 from github import Github
 from github.GithubException import UnknownObjectException
@@ -11,15 +13,28 @@ import github.GithubObject
 from ghmeta import auth
 
 
-def get_release_notes(repo, milestone):
+def get_release_notes(repo_name, milestone_name):
+    """
+    Extracts release notes from Github issues and outputs a Markdown file with
+    extracted information.
+
+        Parameters:
+            repo_name (str): Name of the repo from which to pull milestones/issues
+            milestone_name (str): Name of the milestone from which to pull issues
+    """
+    logger = logging.getLogger(__name__)
+
+    logger.info(
+        f'Creating release notes for repository "{repo_name}" and milestone "{milestone_name}"'
+    )
     ghobj = auth()
 
-    for milestone in ghobj.get_repo(repo).get_milestones():
-        if milestone.title.lower() == "next release":
-            next_release = milestone
+    repo = ghobj.get_repo(repo_name)
+    # Target milestone defaults to 'next release'
+    target_milestone = get_target_milestone_instance(repo, milestone_name)
 
     notes = []
-    for issue in ghobj.get_repo(repo).get_issues(milestone=next_release):
+    for issue in repo.get_issues(milestone=target_milestone):
         meta = {}
         meta["number"] = int(issue.number)
         meta["title"] = issue.title
@@ -28,10 +43,45 @@ def get_release_notes(repo, milestone):
         meta["bug"] = is_bug(issue)
         notes.append(meta)
 
+    logger.info(f"{len(notes)} issue(s) found.")
     create_md_file(notes)
 
 
+def get_target_milestone_instance(repo, milestone_name):
+    """
+    Finds the milestone object that matches the given milestone name
+
+        Parameters:
+            repo (Github.Repository): Instance of a repository class from Github client
+            milestone_name (str): Name of the milestone
+
+        Returns:
+            milestone (Github.Milestone): Instance of the milestone class from Github client
+    """
+
+    for milestone_inst in repo.get_milestones():
+        if milestone_inst.title.lower() == milestone_name.lower():
+            target_milestone = milestone_inst
+            return target_milestone
+
+    # if we do not find the designated milestone
+    raise ValueError(
+        f'No milestone found with the name "{milestone_name}" in repository "{repo.full_name}"'
+    )
+
+
 def create_md_file(notes):
+    """
+    Creates the Markdown file given the extract release notes text and meta information.
+
+        Parameters:
+            notes (List[Dict]): List of dictionaries, each dictionary containing the 
+                                release note information from a single Github issue.
+
+    """
+
+    logger = logging.getLogger(__name__)
+
     notes.sort(key=lambda x: (x["bug"], x["number"]))
     file_dir = os.path.join(
         os.path.dirname(os.path.realpath(__file__)), "release_notes"
@@ -41,20 +91,28 @@ def create_md_file(notes):
     with open(
         filename, "w", encoding="utf-8", errors="xmlcharrefreplace"
     ) as output_file:
+        logger.info(f"Writing release notes to file {filename}")
         output_file.write(headers())
         for row in notes:
             output_file.write(format_row(row))
 
 
 def format_row(row):
+    """
+    Formats a dictionary into a single row of the Markdown table.
+        Parameters:
+            row (Dict): a dictionary representing a single release note
+        Returns:
+            row (str): a formatted string for writing to the MD file
+    """
     row["title"] = md_escape(row["title"])
-    print(row["title"])
     row["body"] = md_escape(row["body"])
     title = "[#{number} {title}]({url})".format(**row)
     return f'| {title} | {row["body"] } |\n'
 
 
 def md_escape(string):
+    """Escape a string for inclusion in a Markdown file."""
     for k, v in escape_table().items():
         string = string.replace(k, v)
     return string
@@ -87,6 +145,7 @@ def headers():
 
 
 def is_bug(issue):
+    """Returns true if an issue include the "bug" label """
     labels = issue.get_labels()
     bug = False
     for l in labels:
@@ -98,6 +157,7 @@ def is_bug(issue):
 
 
 def extract_release_notes(issue):
+    """ Finds the last comment with 'release notes' information in a Github issue chain"""
     comments = issue.get_comments()
 
     release_notes = None
@@ -110,6 +170,7 @@ def extract_release_notes(issue):
 
 
 def format_body(text):
+    """ Formats the text of a given 'release notes' comment """
     text = re.sub(r"\*\*release note[s]?\*\*", "", text)
     text = " ".join(text.split())
     return text.capitalize()
@@ -119,10 +180,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Extract release notes from Github issues and format into a Markdown table."
     )
-    parser.add_argument("--repository", "-r", type=str, help="Repository to scan")
     parser.add_argument(
-        "--milestone", "-m", type=str, help="Name of milestone", default="Next Release"
+        "--repository", "-r", type=str, help="Repository to scan", required=True
+    )
+    parser.add_argument(
+        "--milestone", "-m", type=str, help="Name of milestone", default="next release"
     )
     args = parser.parse_args()
+
+    logging.basicConfig(level=logging.INFO, format="%(message)s")
 
     get_release_notes(args.repository, args.milestone)
